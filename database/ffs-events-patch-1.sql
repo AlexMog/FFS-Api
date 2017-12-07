bf529cf52b4761da86e2497bac168337f4fb1eee
Théo BOUGE <theo.bouge@gmail.com>
2017-12-07 19:08:14 +0100

    UPDATE reference to old column in database/ffs-events-patch-1.sql

--- database/ffs-events-patch-1.sql
+++ database/ffs-events-patch-1.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `events` 
-ADD COLUMN `minimum_views` INT NOT NULL DEFAULT 0 AFTER `ask_for_validation_date`,
+ALTER TABLE `events`
+ADD COLUMN `minimum_views` INT NOT NULL DEFAULT 0 AFTER `is_current`,
 ADD COLUMN `minimum_followers` INT NOT NULL DEFAULT 0 AFTER `minimum_views`;
